9550944b66d718e356014232b6024777caf2bf2a
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-03-05 17:58:52 -0300

    ExtendWith

--- src/test/java/com/example/primeiroteste/MockitoTests.java
+++ src/test/java/com/example/primeiroteste/MockitoTests.java
@@ -2,14 +2,16 @@ package com.example.primeiroteste;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class MockitoTests {
 
     @Mock
